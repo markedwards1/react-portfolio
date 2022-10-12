d42267d86450c10a6f5352870b949bd0758f7ec0
Mark Edwards <mark.francis.edwards@mail.com>
2022-10-12 13:34:30 +0800

    Updates

--- src/containers/Contact/ContactText.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import css from "./ContactText.module.css"
-
-export default function ContactText() {
-  return (
-    <div className={css.contact}>Contact</div>
-  )
-}
--- src/containers/Portfolio/portfolioHeading.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function portfolioHeading() {
-  return (
-    <div>portfolioHeading</div>
-  )
-}
